d35f6aa13fe211ea1a43cd64b1dee04926a91cc5
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-25 11:33:26 +1200

    Remove dotenv
    feat:
    
    - Remove dotenv
    
    - Use direct settings for connection

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,65 +1,17 @@
-require('dotenv').config()
-
 // Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
   client: 'pg',
-  // connection: {
-  //   host: 'localhost',
-  //   user: 'postgres',
-  //   password: 'postgres',
-  //   port: 5432,
-  //   database: 'postgres',
-  // },
-
-  connection: process.env.DATABASE_URL,
-  // connection: 'postgres://postgres:postgres@db:5432/postgres',
-  // pool: {
-  //   min: 2,
-  //   max: 10
-  // },
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    port: 5432,
+    database: 'postgres',
+  },
   migrations: {
     directory: './database/migrations/',
   },
