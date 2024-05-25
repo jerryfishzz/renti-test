dee5dff1453764ba7be5b3cd73249031e32474eb
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-26 10:49:59 +1200

    Create express lib file
    feat:
    
    - Create express lib file for test; refactor index
    
    - Modify log in test to test the testing setup

--- server/src/__tests__/feature/accounts.test.ts
+++ server/src/__tests__/feature/accounts.test.ts
@@ -1,7 +1,14 @@
+import request from 'supertest'
+
 import Account from './models/account'
+import app from '../../lib/express'
 
 test(`log in`, async () => {
-  const account = new Account()
-  let json = await account.readByUsername('booklover1')
-  expect(json.userName).toEqual('booklover1')
+  // const account = new Account()
+  // let json = await account.readByUsername('booklover1')
+  // expect(json.userName).toEqual('booklover1')
+
+  const response = await request(app).get('/')
+  expect(response.status).toBe(200)
+  expect(response.text).toEqual('Hello, World!')
 })
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,20 +2,10 @@ if (process.env.NODE_ENV === 'production') {
   require('module-alias/register')
 }
 
-import express from 'express'
-import cors from 'cors'
-
+import app from 'lib/express'
 import routes from './routes'
 
-const app = express()
 const port = process.env.PORT || 6666
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
