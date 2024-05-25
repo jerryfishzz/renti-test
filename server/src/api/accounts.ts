3e9e9d572cd8f803f172c25732e69fd77847304b
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-25 18:03:54 +1200

    Validate get account by id
    feat:
    
    - Create account schema for the endpoint
    
    - Validate the endpoint by its schema

--- server/src/api/accounts.ts
+++ server/src/api/accounts.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
 import { db } from 'lib/db'
+import validate from 'lib/validate'
+import { getById } from 'schemas/account.schema'
 
 const router = Router({ mergeParams: true })
 
-router.get('/accounts/:id', async (req, res) => {
+router.get('/accounts/:id', validate(getById), async (req, res) => {
   try {
     const account = await db('accounts').where('id', req.params.id).first()
     console.log('account')
---
+++ server/src/schemas/account.schema.ts
@@ -0,0 +1,53 @@
+import { z } from 'zod'
+import type { Request, Response } from 'express'
+
+import { Account } from 'types/db'
+
+// const queryParams = z.object({
+//   id: z.number().optional(),
+//   username: z.string().optional(),
+// })
+
+// export const search = z.object({ params: queryParams })
+// export type SearchRequest = Request<
+//   z.infer<typeof search>['params'],
+//   unknown,
+//   unknown,
+//   Pagination
+// >
+// export type SearchResponse = Response<PaginationResult<Account>>
+
+export const getById = z.object({ params: z.object({ id: z.coerce.number() }) })
+export type GetByIdRequest = Request<z.infer<typeof getById>['params']>
+export type GetByIdResponse = Response<Account>
+
+// export const Post = z.object({
+//   body: Account.omit({ id: true, created_at: true, updated_at: true }),
+// })
+// export type PostRequest = Request<
+//   unknown,
+//   unknown,
+//   z.infer<typeof Post>['body']
+// >
+// export type PostResponse = Response<Partial<DBAccount>>
+
+// export const Patch = Get.merge(z.object({ body: Account.partial() }))
+// export type PatchRequest = Request<
+//   z.infer<typeof Patch>['params'],
+//   z.infer<typeof Patch>['body']
+// >
+// export type PatchResponse = Response
+
+// export const Delete = Get
+// export type DeleteRequest = Request<z.infer<typeof Delete>['params']>
+// export type DeleteResponse = Response
+
+// export const Login = z.object({
+//   body: Account.pick({ username: true, password: true }),
+// })
+// export type LoginRequest = Request<
+//   unknown,
+//   unknown,
+//   z.infer<typeof Login>['body']
+// >
+// export type LoginResponse = Response<{ access_token: string }>
