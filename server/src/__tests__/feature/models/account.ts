1fb91ae10e87e298714e1a4bf765e90800a2d6b0
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-26 17:10:48 +1200

    Update account.ts
    feat: modify the code to adapt new query and jsonQuery functions; remove unused code

--- server/src/__tests__/feature/models/account.ts
+++ server/src/__tests__/feature/models/account.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker'
+import TestAgent from 'supertest/lib/agent'
 
 import { query, jsonQuery, merge } from './util'
 import { Account as DBAccount } from 'types/db'
@@ -6,17 +7,16 @@ import { Account as DBAccount } from 'types/db'
 export default class Account {
   private _id: DBAccount['id'] | null
   // private _username: DBAccount['username'] | null
-  // private _settings: any
   private _json: Partial<DBAccount>
-  // employments: any[]
+  private _agent: TestAgent
 
   constructor(
+    agent: TestAgent,
     json: Omit<DBAccount, 'created_at' | 'updated_at'> | null = null
   ) {
     this._id = null
+    this._agent = agent
     // this._username = null
-    // this._settings = null
-    // this.employments = []
 
     if (json) {
       this._id = json.id
@@ -25,16 +25,13 @@ export default class Account {
       if (this._json.hasOwnProperty('id')) {
         delete this._json.id
       }
-      // this._settings = new CompanySettings(json.id, json.settings)
     } else {
-      // this._settings = new CompanySettings()
       this._json = {
         name: faker.person.fullName(),
         username: faker.internet.userName(),
         password: faker.internet.password(),
         email: faker.internet.email(),
         reading_preferences: [],
-        // settings: structuredClone(this._settings.json),
       }
     }
   }
@@ -51,52 +48,51 @@ export default class Account {
     return this._json
   }
 
-  // get settings() {
-  //   return this._settings
-  // }
-
   async create(attrs = {}) {
     let json = structuredClone(this._json)
     const json_data = merge(attrs, json)
     this._json = json_data
 
-    const result = await jsonQuery('/accounts', {
-      method: 'post',
-      body: JSON.stringify({ ...attrs, ...json }),
-    })
+    const result = await jsonQuery(
+      '/accounts',
+      {
+        method: 'post',
+        body: { ...attrs, ...json },
+      },
+      this._agent
+    )
 
     this._id = result.id
-    // this._settings.company_id = result.id
 
     return result
   }
 
   async read() {
-    return jsonQuery(`/accounts/${this._id}`)
+    return jsonQuery(`/accounts/${this._id}`, {}, this._agent)
   }
 
-  async readByUsername(username: DBAccount['username']) {
-    const result = await jsonQuery(`/accounts/username/${username}`)
+  // async readByUsername(username: DBAccount['username']) {
+  //   const result = await jsonQuery(`/accounts/username/${username}`)
 
-    this._id = result.id
-    this._json = structuredClone(result)
-    if (this._json.hasOwnProperty('id')) {
-      delete this._json.id
-    }
+  //   this._id = result.id
+  //   this._json = structuredClone(result)
+  //   if (this._json.hasOwnProperty('id')) {
+  //     delete this._json.id
+  //   }
 
-    return result
-  }
+  //   return result
+  // }
 
-  async update(change: any) {
-    merge(change, this._json)
-    const req = await query(`/accounts/${this._id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(change),
-    })
-    return req
-  }
+  // async update(change: any) {
+  //   merge(change, this._json)
+  //   const req = await query(`/accounts/${this._id}`, {
+  //     method: 'PATCH',
+  //     body: JSON.stringify(change),
+  //   })
+  //   return req
+  // }
 
   async delete() {
-    return query(`/accounts/${this._id}`, { method: 'delete' })
+    return query(`/accounts/${this._id}`, { method: 'delete' }, this._agent)
   }
 }
