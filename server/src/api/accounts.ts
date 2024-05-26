6c52d6438547e069a1846044a8fab66501dfbd1e
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-26 17:37:30 +1200

    Remove try catch blocks
    test: remove try catch blocks in the accounts api

--- server/src/api/accounts.ts
+++ server/src/api/accounts.ts
@@ -25,15 +25,10 @@ router.get(
   '/accounts/:id',
   validate(getById),
   guard(async (req: GetByIdRequest, res: GetByIdResponse) => {
-    try {
-      const account = await db('accounts').where('id', req.params.id).first()
-      if (!account) return res.sendStatus(404)
+    const account = await db('accounts').where('id', req.params.id).first()
+    if (!account) return res.sendStatus(404)
 
-      return res.send(account)
-    } catch (error) {
-      console.error(error)
-      throw error
-    }
+    return res.send(account)
   })
 )
 
@@ -41,17 +36,12 @@ router.get(
   '/accounts/username/:username',
   validate(getByUsername),
   guard(async (req: GetByUsernameRequest, res: GetByUsernameResponse) => {
-    try {
-      const account = await db('accounts')
-        .where('username', req.params.username)
-        .first()
-      if (!account) return res.sendStatus(404)
+    const account = await db('accounts')
+      .where('username', req.params.username)
+      .first()
+    if (!account) return res.sendStatus(404)
 
-      return res.send(account)
-    } catch (error) {
-      console.error(error)
-      throw error
-    }
+    return res.send(account)
   })
 )
 
