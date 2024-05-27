2abdf9533053fea661cc018a9d1dbe4c7738bade
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-27 23:07:29 +1200

    Create SignIn Page
    feat: Create SignIn Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
 import React, { useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import SignIn from './pages/SignIn'
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('/accounts/1')
-      console.log(response)
-      const data = await response.json()
-      console.log(data)
-    }
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch('/accounts/1')
+  //     console.log(response)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
+  //   fetchData()
+  // }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <SignIn />
   )
 }
 
