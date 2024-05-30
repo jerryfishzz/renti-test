e6a0c2f62df27d74a24ba008d6c2f8309258840b
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-30 20:06:32 +1200

    Remove default export
    bug: remove default export due to some unknown weird problems by default exporting function

--- client/src/lib/context.ts
+++ client/src/lib/context.ts
@@ -1,13 +1,13 @@
 import { createContext as reactCreateContext, useContext } from 'react'
 
-export default function createContext<A>() {
+export function createContext<A>() {
   const ctx = reactCreateContext<A | undefined>(undefined)
 
   function useCtx() {
     const c = useContext(ctx)
     if (c === undefined)
       throw new Error(
-        `Component must be inside the context provider with a value`
+        `Component must be inside the context provider with a value`,
       )
     return c
   }
