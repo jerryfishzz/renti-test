bc2c6fbab9c7af06c1e162329230103b80815781
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-01 16:55:47 +1200

    Turn router as hook
    feat:
    
    - Create a hook to provide router with loaders
    
    - Use useRouter in wrapper

--- client/src/RouterWrapper.tsx
+++ client/src/RouterWrapper.tsx
@@ -1,7 +1,9 @@
 import { RouterProvider } from 'react-router-dom'
 
-import router from 'router'
+import { useRouter } from 'router'
 
 export default function RouterWrapper() {
+  const router = useRouter()
+
   return <RouterProvider router={router} />
 }
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,23 +1,48 @@
 import {
+  LoaderFunctionArgs,
   Route,
   createBrowserRouter,
   createRoutesFromElements,
+  redirect,
 } from 'react-router-dom'
 
 import SignIn from './pages/SignIn'
 import Library from './pages/Library'
 import Clubs from './pages/Clubs'
 import User from './pages/User'
+import { useAuth } from 'contexts/auth'
+import { useCallback } from 'react'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route index element={<Library />} />
-      <Route path="clubs" element={<Clubs />} />
-      <Route path="user" element={<User />} />
-      <Route path="sign-in" element={<SignIn />} />
-    </Route>
+export function useRouter() {
+  const { user } = useAuth()
+
+  const loginLoader = useCallback(() => {
+    if (!user) return null
+    return redirect('/')
+  }, [user])
+
+  const protectedLoader = useCallback(
+    ({ request }: LoaderFunctionArgs) => {
+      if (!user) {
+        const params = new URLSearchParams()
+        params.set('from', new URL(request.url).pathname)
+        return redirect('/sign-in?' + params.toString())
+      }
+      return null
+    },
+    [user],
   )
-)
 
-export default router
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<Library />} loader={protectedLoader} />
+        <Route path="clubs" element={<Clubs />} loader={protectedLoader} />
+        <Route path="user" element={<User />} loader={protectedLoader} />
+        <Route path="sign-in" element={<SignIn />} loader={loginLoader} />
+      </Route>,
+    ),
+  )
+
+  return router
+}
