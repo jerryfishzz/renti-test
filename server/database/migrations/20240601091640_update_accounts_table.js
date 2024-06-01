d20b5382cd94fa6b5bea2a05af163522120ff720
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-01 21:21:37 +1200

    Create 20240601091640_update_accounts_table.js
    feat: make username col in accounts table unique

---
+++ server/database/migrations/20240601091640_update_accounts_table.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.table('accounts', function (table) {
+    table.unique('username')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.table('accounts', function (table) {
+    table.string('username').notNullable()
+  })
+}
