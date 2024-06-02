117a369368f828bdd1bba062cdeac01f9702c96c
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-02 18:32:22 +1200

    Create useValidation.ts
    feat: create the hook to help handle errors

---
+++ client/src/hooks/useValidation.ts
@@ -0,0 +1,36 @@
+import { useMemo } from 'react'
+import { z } from 'zod'
+
+import { useAuth } from 'contexts/auth'
+
+type Query = (logout: () => void, location: Location) => ValidatedQuery
+export type ValidatedQuery = <T extends Record<string, unknown>>(
+  url: string,
+  schema: z.Schema<T> | null,
+  data: T,
+) => Promise<any>
+
+// Use on AuthProvider
+export function useAuthValidation(query: Query, logout: () => void) {
+  const location = window.location
+
+  const validatedQuery = useMemo(
+    () => query(logout, location),
+    [location, logout, query],
+  )
+
+  return validatedQuery
+}
+
+// Use inside AuthProvider
+export function useValidation(query: Query) {
+  const location = window.location
+  const { logout } = useAuth()
+
+  const validatedQuery = useMemo(
+    () => query(logout, location),
+    [location, logout, query],
+  )
+
+  return validatedQuery
+}
