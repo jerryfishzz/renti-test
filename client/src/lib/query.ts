eb2c5cc2f75ab9b61ac49db7fda5047327c6a49b
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-02 18:33:07 +1200

    Modify post
    feat: modify post to fit useValidation hook

--- client/src/lib/query.ts
+++ client/src/lib/query.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { redirect } from 'react-router-dom'
 
 import { getUserFromLocalStorage } from 'contexts/auth'
 import { validate } from './validate'
@@ -13,7 +14,6 @@ const API_BASE_URL =
 const baseQuery = (url: string, options: RequestInit = {}) => {
   const user = getUserFromLocalStorage()
 
-  // TODO: Add error handling for no user info
   return fetch(`${API_BASE_URL}${url}`, {
     ...options,
     headers: {
@@ -23,25 +23,44 @@ const baseQuery = (url: string, options: RequestInit = {}) => {
   })
 }
 
-async function post<T extends Record<string, unknown>>(
-  url: string,
-  schema: z.Schema<T> | null,
-  body: T,
-) {
-  try {
-    const validatedResult = schema ? validate(schema, body) : body
+function post(logout: () => void, location: Location) {
+  return async <T extends Record<string, unknown>>(
+    url: string,
+    schema: z.Schema<T> | null,
+    data: T,
+  ) => {
+    try {
+      const validatedResult = schema ? validate(schema, data) : data
 
-    const response = await baseQuery(url, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(validatedResult),
-    })
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error(error)
+      const response = await baseQuery(url, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(validatedResult),
+      })
+
+      if (!response.ok) {
+        if (response.status === 403) {
+          logout()
+          console.log(location.pathname)
+
+          const params = new URLSearchParams()
+          params.set('from', '')
+          redirect('/sign-in?' + params.toString())
+          throw new Error('Invalid username or password')
+        }
+
+        throw new Error(response.statusText)
+      }
+
+      const result = await response.json()
+      console.log(result)
+      return result
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
   }
 }
 
