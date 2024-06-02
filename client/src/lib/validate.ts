54773f9042821dd9fe07206a9dba284fc5ed2b73
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-02 14:34:07 +1200

    Add return value
    bug: Add return value

--- client/src/lib/validate.ts
+++ client/src/lib/validate.ts
@@ -5,6 +5,7 @@ export function validate(schema: z.Schema, data: unknown) {
   try {
     const result = schema.safeParse(data)
     if (!result.success) throw result.error
+    return result.data
   } catch (e) {
     const validationError = fromZodError(e as z.ZodError)
     throw Error(validationError.message)
