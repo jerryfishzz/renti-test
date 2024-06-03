da95b97f2fc5412ce5f46979677afa3da2218bae
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-03 23:33:35 +1200

    Modify get books api
    feat: Modify get books api ton return status if it has for the current user

--- server/src/api/books.ts
+++ server/src/api/books.ts
@@ -2,20 +2,38 @@ import type { Request } from 'express'
 
 import { db } from 'lib/db'
 import { guard, router } from './utils'
-import { GetBooksResponse } from 'schemas/book.schema'
+import { GetBooksResponse, GetBooksReturn } from 'schemas/book.schema'
 import { auth } from 'lib/jwt'
+import { Book, Genre } from 'types/db'
 
 router.get(
-  '/books',
+  '/books/account/:id',
   auth(),
   guard(async (req: Request, res: GetBooksResponse) => {
-    const books = await db('books')
+    const books = (await db('books')
       .select('books.*', 'genres.name as genre')
-      .join('genres', 'books.genre_id', 'genres.id')
-
+      .join('genres', 'books.genre_id', 'genres.id')) as Awaited<
+      (Book & { genre: Genre['name'] })[]
+    >
     if (!books) return res.sendStatus(404)
 
-    return res.send(books)
+    const lists = await db('reading_lists').where('account_id', req.params.id)
+    if (!lists) return res.sendStatus(404)
+
+    const myBooks: GetBooksReturn[] = []
+    for (const list of lists) {
+      const book = await db('books').where('id', list.book_id).first()
+      if (!book) return res.sendStatus(500)
+
+      const genre = await db('genres').where('id', book.genre_id).first()
+      if (!genre) return res.sendStatus(500)
+
+      myBooks.push({ ...book, status: list.status, genre: genre.name })
+    }
+
+    const notMyBooks = books.filter(book => book.id !== Number(req.params.id))
+
+    return res.send([...myBooks, ...notMyBooks])
   })
 )
 
--- server/src/schemas/book.schema.ts
+++ server/src/schemas/book.schema.ts
@@ -1,5 +1,14 @@
 import type { Response } from 'express'
+import { z } from 'zod'
 
-import { Book } from 'types/db'
+import { Book, Genre, Reading_List } from 'types/db'
 
-export type GetBooksResponse = Response<(Book & { genre: string })[]>
+export const getBooks = z.object({
+  params: z.object({ id: z.coerce.number() }),
+})
+export type GetBooksRequest = Response<z.infer<typeof getBooks>['params']>
+export type GetBooksReturn = Book & {
+  genre: Genre['name']
+  status?: Reading_List['status']
+}
+export type GetBooksResponse = Response<GetBooksReturn[]>
