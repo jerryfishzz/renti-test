82199412cb8416d1538e104b57c87ff2ab158fb0
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-03 21:42:04 +1200

    Modify return types
    feat: Modify return types of my list get request

--- server/src/api/reading_lists.ts
+++ server/src/api/reading_lists.ts
@@ -4,6 +4,7 @@ import { auth } from 'lib/jwt'
 import {
   GetByIdResponse,
   GetByUserIdRequest,
+  GetByIdReturn,
 } from 'schemas/reading_list.schema'
 
 router.get(
@@ -13,7 +14,18 @@ router.get(
     const lists = await db('reading_lists').where('account_id', req.params.id)
     if (!lists) return res.sendStatus(404)
 
-    return res.send(lists)
+    const listedBooks: GetByIdReturn[] = []
+    for (const list of lists) {
+      const book = await db('books').where('id', list.book_id).first()
+      if (!book) return res.sendStatus(500)
+
+      const genre = await db('genres').where('id', book.genre_id).first()
+      if (!genre) return res.sendStatus(500)
+
+      listedBooks.push({ ...book, status: list.status, genre: genre.name })
+    }
+
+    return res.send(listedBooks)
   })
 )
 
--- server/src/schemas/reading_list.schema.ts
+++ server/src/schemas/reading_list.schema.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod'
 import type { Request, Response } from 'express'
 
-import { Reading_List } from 'types/db'
+import { Book, Genre, Reading_List } from 'types/db'
 
 export const getByUserId = z.object({
   params: z.object({ id: z.coerce.number() }),
 })
+export type GetByIdReturn = Book &
+  Pick<Reading_List, 'status'> & { genre: Genre['name'] }
 export type GetByUserIdRequest = Request<z.infer<typeof getByUserId>['params']>
-export type GetByIdResponse = Response<Reading_List[]>
+export type GetByIdResponse = Response<GetByIdReturn[]>
