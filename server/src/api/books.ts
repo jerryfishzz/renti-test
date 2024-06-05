cc9f3f44ffceb26ca33f4fe9b52cb8bf3be57e7b
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-05 20:29:27 +1200

    Rename getBooks
    refactor:
    
    - Rename getBooks and related variables
    
    bug:
    
    - Correct the request type to Request

--- server/src/api/books.ts
+++ server/src/api/books.ts
@@ -1,47 +1,54 @@
-import type { Request } from 'express'
-
 import { db } from 'lib/db'
 import { guard, router } from './utils'
-import { GetBooksResponse, GetBooksReturn } from 'schemas/book.schema'
+import {
+  GetBooksByAccountIdRequest,
+  GetBooksByAccountIdResponse,
+  GetBooksByAccountIdReturn,
+} from 'schemas/book.schema'
 import { auth } from 'lib/jwt'
 import { Book, Genre } from 'types/db'
 
 router.get(
   '/books/account/:id',
   auth(),
-  guard(async (req: Request, res: GetBooksResponse) => {
-    const books = (await db('books')
-      .select('books.*', 'genres.name as genre')
-      .join('genres', 'books.genre_id', 'genres.id')) as Awaited<
-      (Book & { genre: Genre['name'] })[]
-    >
-    if (!books) return res.sendStatus(404)
+  guard(
+    async (
+      req: GetBooksByAccountIdRequest,
+      res: GetBooksByAccountIdResponse
+    ) => {
+      const books = (await db('books')
+        .select('books.*', 'genres.name as genre')
+        .join('genres', 'books.genre_id', 'genres.id')) as Awaited<
+        (Book & { genre: Genre['name'] })[]
+      >
+      if (!books) return res.sendStatus(404)
 
-    const lists = await db('reading_lists').where('account_id', req.params.id)
-    if (!lists) return res.sendStatus(404)
+      const lists = await db('reading_lists').where('account_id', req.params.id)
+      if (!lists) return res.sendStatus(404)
 
-    const myBooks: GetBooksReturn[] = []
-    for (const list of lists) {
-      const book = await db('books').where('id', list.book_id).first()
-      if (!book) return res.sendStatus(500)
+      const myBooks: GetBooksByAccountIdReturn[] = []
+      for (const list of lists) {
+        const book = await db('books').where('id', list.book_id).first()
+        if (!book) return res.sendStatus(500)
 
-      const genre = await db('genres').where('id', book.genre_id).first()
-      if (!genre) return res.sendStatus(500)
+        const genre = await db('genres').where('id', book.genre_id).first()
+        if (!genre) return res.sendStatus(500)
 
-      myBooks.push({ ...book, status: list.status, genre: genre.name })
+        myBooks.push({ ...book, status: list.status, genre: genre.name })
+      }
+
+      const bookIdSet = new Set<number>()
+      const wantedBooks: GetBooksByAccountIdReturn[] = []
+      const allBooks = [...myBooks, ...books]
+      for (const book of allBooks) {
+        if (bookIdSet.has(book.id)) continue
+        bookIdSet.add(book.id)
+        wantedBooks.push(book)
+      }
+
+      return res.send(wantedBooks)
     }
-
-    const bookIdSet = new Set<number>()
-    const wantedBooks: GetBooksReturn[] = []
-    const allBooks = [...myBooks, ...books]
-    for (const book of allBooks) {
-      if (bookIdSet.has(book.id)) continue
-      bookIdSet.add(book.id)
-      wantedBooks.push(book)
-    }
-
-    return res.send(wantedBooks)
-  })
+  )
 )
 
 export { router as books }
--- server/src/schemas/book.schema.ts
+++ server/src/schemas/book.schema.ts
@@ -1,14 +1,16 @@
-import type { Response } from 'express'
+import type { Request, Response } from 'express'
 import { z } from 'zod'
 
 import { Book, Genre, Reading_List } from 'types/db'
 
-export const getBooks = z.object({
+export const getBooksByAccountId = z.object({
   params: z.object({ id: z.coerce.number() }),
 })
-export type GetBooksRequest = Response<z.infer<typeof getBooks>['params']>
-export type GetBooksReturn = Book & {
+export type GetBooksByAccountIdRequest = Request<
+  z.infer<typeof getBooksByAccountId>['params']
+>
+export type GetBooksByAccountIdReturn = Book & {
   genre: Genre['name']
   status?: Reading_List['status']
 }
-export type GetBooksResponse = Response<GetBooksReturn[]>
+export type GetBooksByAccountIdResponse = Response<GetBooksByAccountIdReturn[]>
