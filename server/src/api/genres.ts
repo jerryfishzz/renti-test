c402934e686dddd291f0c9f8a892a41b88033670
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-09 22:59:30 +1200

    Replace duplicates
    refactor: replace duplicate types

--- server/src/api/genres.ts
+++ server/src/api/genres.ts
@@ -1,13 +1,13 @@
 import { db } from 'lib/db'
 import { guard, router } from './utils'
 import validate from 'lib/validate'
-import { GetByIdResponse } from 'schemas/genre.schema'
+import { ResponseGenre } from 'schemas/genre.schema'
 import { GetParamsIdRequest, getParamsId } from 'schemas/shared.schema'
 
 router.get(
   '/genres/:id',
   validate(getParamsId),
-  guard(async (req: GetParamsIdRequest, res: GetByIdResponse) => {
+  guard(async (req: GetParamsIdRequest, res: ResponseGenre) => {
     const genre = await db('genres').where('id', req.params.id).first()
     if (!genre) return res.sendStatus(404)
 
--- server/src/schemas/genre.schema.ts
+++ server/src/schemas/genre.schema.ts
@@ -4,7 +4,3 @@ import type { Request, Response } from 'express'
 import { Genre } from 'types/db'
 
 export type ResponseGenre = Response<Genre>
-
-export const getById = z.object({ params: z.object({ id: z.coerce.number() }) })
-export type GetByIdRequest = Request<z.infer<typeof getById>['params']>
-export type GetByIdResponse = ResponseGenre
