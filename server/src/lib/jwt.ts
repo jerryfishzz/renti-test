344c8af6e1d4415c4cfb3f1fbc841fca851fedfa
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-06-16 20:48:58 +1200

    Use private and public keys both
    feat:
    
    - Modify getJwtMaterial to get private or public conditionally
    
    - Use public key to verify

--- server/src/lib/jwt.ts
+++ server/src/lib/jwt.ts
@@ -9,10 +9,12 @@ const {
   JWT_PUBLIC_KEY_MATERIAL,
 } = process.env
 
-function getJwtMaterial(type: 'public' | 'private' = 'private') {
+type Key = 'public' | 'private'
+
+function getJwtMaterial(key: Key = 'private') {
   let jwtMaterialBuffer: Buffer
 
-  if (type === 'private') {
+  if (key === 'private') {
     if (JWT_PRIVATE_KEY_MATERIAL) {
       jwtMaterialBuffer = Buffer.from(JWT_PRIVATE_KEY_MATERIAL)
     } else if (JWT_PRIVATE_KEY_PATH) {
@@ -48,8 +50,8 @@ export const sign = (payload: Payload) => {
   return jwt.sign(payload, jwtMaterial, { algorithm: 'RS256' })
 }
 
-export const verify = (access_token: string) => {
-  const jwtMaterial = getJwtMaterial()
+export const verify = (access_token: string, key: Key = 'public') => {
+  const jwtMaterial = getJwtMaterial(key)
   return jwt.verify(access_token, jwtMaterial)
 }
 
