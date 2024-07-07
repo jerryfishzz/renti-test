8b150b82bab9f4b1a920f2c1352a6421f3b68e18
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-07-07 20:24:38 +1200

    Create getById service
    feat: Create getById service under account service

--- server/src/__tests__/integration/services/account.service.ts
+++ server/src/__tests__/integration/services/account.service.ts
@@ -1,6 +1,10 @@
 import { Account } from 'types/db'
 import { jsonQuery } from '../utils'
 
+export function getById(id: number): Promise<Account[]> {
+  return jsonQuery({ path: `/accounts/${id}` })
+}
+
 export function readList(): Promise<Account[]> {
   return jsonQuery({ path: '/accounts' })
 }
