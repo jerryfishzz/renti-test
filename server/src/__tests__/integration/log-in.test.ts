42736d973c04252fcb4f4a55b5a37e4f8b1ccce2
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-07-09 19:24:05 +1200

    Add sessionId
    test: add sessionId and clear method

--- server/src/__tests__/integration/log-in.test.ts
+++ server/src/__tests__/integration/log-in.test.ts
@@ -4,9 +4,14 @@ import * as AccountService from './services/account.service'
 import * as SessionService from './services/session.service'
 import { db } from 'lib/db'
 
+let sessionId: number | null = null
 const { API_USER, API_PASS } = process.env
 
 afterAll(async () => {
+  if (sessionId) {
+    await SessionService.deleteById(sessionId)
+    sessionId = null
+  }
   await db.destroy()
 })
 
@@ -14,8 +19,6 @@ describe('log in', () => {
   describe('given the username does not exist', () => {
     it('should return 403', async () => {
       const { username, password } = AccountService.createMockAccount()
-      console.log(username)
-      console.log(password)
 
       const { statusCode } = await AccountService.logIn(username, password)
 
