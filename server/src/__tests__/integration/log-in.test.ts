32c98a1290acbcd369464dff17ab2426e19e79d3
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-07-12 11:17:38 +1200

    Create login logic
    test:
    
    - Create mock for date-fns
    
    - Move session id to login logic and create one of login logic tests

--- server/src/__tests__/integration/log-in.test.ts
+++ server/src/__tests__/integration/log-in.test.ts
@@ -1,17 +1,27 @@
 import { faker } from '@faker-js/faker'
+import { addDays, addHours } from 'date-fns'
 
 import * as AccountService from './services/account.service'
 import * as SessionService from './services/session.service'
 import { db } from 'lib/db'
+import { AccountReturn } from 'schemas/account.schema'
 
-let sessionId: number | null = null
+jest.setTimeout(1000 * 60 * 10)
+
+jest.mock('date-fns', () => ({
+  ...jest.requireActual('date-fns'),
+  addDays: jest.fn(),
+  addHours: jest.fn(),
+}))
+
 const { API_USER, API_PASS } = process.env
 
+const accessExpDate = new Date()
+accessExpDate.setHours(accessExpDate.getHours() + 1)
+const refreshExpDate = new Date()
+refreshExpDate.setDate(refreshExpDate.getDate() + 14)
+
 afterAll(async () => {
-  if (sessionId) {
-    await SessionService.deleteById(sessionId)
-    sessionId = null
-  }
   await db.destroy()
 })
 
@@ -34,6 +44,60 @@ describe('log in', () => {
       )
 
       expect(statusCode).toBe(403)
+    })
+  })
+
+  describe('login logic', () => {
+    let username: string = ''
+    let password: string = ''
+    let account: AccountReturn | null = null
+    let sessionId: number | null = null
+
+    beforeEach(async () => {
+      // @ts-ignore
+      addHours.mockReturnValue(accessExpDate)
+      // @ts-ignore
+      addDays.mockReturnValue(refreshExpDate)
+
+      const newAccount = AccountService.createMockAccount()
+      username = newAccount.username
+      password = newAccount.password
+
+      // Log in as admin
+      const { body } = await AccountService.create(newAccount)
+      account = body
+    })
+
+    afterEach(async () => {
+      if (sessionId) {
+        await SessionService.deleteById(sessionId)
+        sessionId = null
+      }
+
+      // Need to delete after session is deleted since it's a foreign key
+      if (account) {
+        await AccountService.deleteById(account.id)
+        account = null
+      }
+      username = ''
+      password = ''
+    })
+
+    describe('given the account logs in first time', () => {
+      it('should return session id, user info, and access token', async () => {
+        const { statusCode, body } = await AccountService.logIn(
+          username,
+          password
+        )
+        sessionId = body.sessionId
+
+        expect(statusCode).toBe(200)
+        expect(body).toEqual({
+          ...account,
+          access_token: expect.any(String),
+          sessionId: expect.any(Number),
+        })
+      })
     })
   })
 })
