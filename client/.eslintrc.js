d81c491e34e1c6f9a924ebc70902f66ea2c84743
Jerry Zhang <jerryfishzz@gmail.com>
2025-06-15 18:02:18 +1200

    Modify .eslintrc.js
    feat: modify .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,19 +5,23 @@ module.exports = {
     es2021: true,
     jest: true,
   },
+  parser: '@typescript-eslint/parser',
   extends: [
-    'react-app',
+    'react-app', // React hooks and other React-specific rules
     'react-app/jest',
     'plugin:@typescript-eslint/recommended', // typescript-eslint
     'plugin:react/recommended', // eslint-plugin-react
     'plugin:react/jsx-runtime', // avoid react scope errors
     'plugin:prettier/recommended', // eslint-plugin-prettier
-    'prettier', // prettier
+    'prettier', // eslint-config-prettier
   ],
-  plugins: ['react', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   settings: {
     react: {
       version: 'detect',
     },
+  },
+  rules: {
+    'prettier/prettier': 1, // Default value is 2 (error)
   },
 }
